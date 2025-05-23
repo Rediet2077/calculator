e9f0f2357d6b87ee9f5b29195727cf002bd984b8
asaye <Asayemax1921@gmail.com>
2025-05-23 15:51:38 -0300

    add sine function

---
+++ addsine.java
@@ -0,0 +1,7 @@
+case 1: // Sine
+                    System.out.print("Enter angle in degrees: ");
+                     System.out.print("Enter angle in degrees: ");
+                    
+                    double angle = scanner.nextDouble();
+                    System.out.println("sin(" + angle + "°) = " + Math.sin(Math.toRadians(angle)));
+                    break;
