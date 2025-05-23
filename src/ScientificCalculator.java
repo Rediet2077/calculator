cd175a1c40281238c5a2d92d75da5254a97e2918
Sosi mulatu <sosi24898@gmail.com>
2025-05-23 20:57:59 +0100

    Update ScientificCalculator.java

--- src/ScientificCalculator.java
+++ src/ScientificCalculator.java
@@ -1,1 +1,10 @@
 
+case 5: 
+                    System.out.print("Enter positive number: ");
+                    num = scanner.nextDouble();
+                    if (num > 0) {
+                        System.out.println("log(" + num + ") = " + Math.log10(num));
+                    } else {
+                        System.out.println("Invalid input! Number must be positive.");
+                    }
+                    break;
