888063820d2424b0638f1972017e11163be0eb04
asaye <Asayemax1921@gmail.com>
2025-05-23 16:03:25 -0300

    merge Beti to dev

--- src/powere.java
+++ src/powere.java
@@ -1,3 +1,10 @@
+case 1: // Sine
+                    System.out.print("Enter angle in degrees: ");
+                     System.out.print("Enter angle in degrees: ");
+                    
+                    double angle = scanner.nextDouble();
+                    System.out.println("sin(" + angle + "°) = " + Math.sin(Math.toRadians(angle)));
+                    break;
 case 7:
                     System.out.print("Enter base: ");
                     double base = scanner.nextDouble();
