95d7850e1457571c568db564ba70411107eb678e
asaye <Asayemax1921@gmail.com>
2025-05-23 16:04:42 -0300

    merge Beti to dev

--- addsine - Copy.java
+++
@@ -1,7 +0,0 @@
-case 1: // Sine
-                    System.out.print("Enter angle in degrees: ");
-                     System.out.print("Enter angle in degrees: ");
-                    
-                    double angle = scanner.nextDouble();
-                    System.out.println("sin(" + angle + "°) = " + Math.sin(Math.toRadians(angle)));
-                    break;
