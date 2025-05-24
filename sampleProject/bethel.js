3f441a7d1b12e90c31a6956cf1bfb592daa2c2dd
betel1221 <betelhem453@gmail.com>
2025-05-24 08:52:05 -0700

    Finished updates on beti branch

--- sampleProject/bethel.js
+++ sampleProject/bethel.js
@@ -1,1 +1,5 @@
-console.log("This is Bethel's Branch")
+case 3: // Tangent
+                    System.out.print("Enter angle in degrees: ");
+                    angle = scanner.nextDouble();
+                    System.out.println("tan(" + angle + "°) = " + Math.tan(Math.toRadians(angle)));
+                    break; 
