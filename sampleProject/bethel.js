839c917bd570d64f21facc097bc2eb6aa2f87f77
betel1221 <betelhem453@gmail.com>
2025-05-25 02:46:27 -0700

    unwanted file deleted

--- sampleProject/bethel.js
+++
@@ -1,5 +0,0 @@
-case 3: // Tangent
-                    System.out.print("Enter angle in degrees: ");
-                    angle = scanner.nextDouble();
-                    System.out.println("tan(" + angle + "°) = " + Math.tan(Math.toRadians(angle)));
-                    break; 
